2ad9f7c0517c2430401c13ab9c3462fbf80da538
DevAaqui <aaquibrais12345@gmail.com>
2023-01-02 00:12:55 +0530

    Usng localhst & display input text by reading file

--- Node Ch-1-creating server/app2.js
+++ Node Ch-1-creating server/app2.js
@@ -1,38 +1,54 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
     //console.log(req.url, req.method, req.headers)
     const url = req.url
-    if(url==='/home')
+    const method = req.method
+    if(url==='/')
     {
-    res.write('<html>')
-    res.write('<head><title>First Program</title></head>')
-    res.write('<body><h1>Welcome Home</h1></body>')
-    res.write('</html')
-    return res.end()
+        fs.readFile('message.txt',{encoding: "utf-8"},(err,data)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log(data)
+            res.write('<html>')
+            res.write('<head><title>First Message</title></head>')
+            res.write(`<body>${data}</body>`)
+            res.write('<body><form action="/message" method="POST"><input type="text" name="msg"><button type="submit">Send</button</form></body>')
+            res.write('</html')
+            return res.end()
+
+        })
+    
     }
-    else  if(url==='/about')
+    if(url === '/message' && method ==='POST')
     {
-    res.write('<html>')
-    res.write('<head><title>First Program</title></head>')
-    res.write('<body><h1>Welcome to About Us Page</h1></body>')
-    res.write('</html')
-    return res.end()
+        const body = []
+        req.on('data', (chunk)=>{
+           console.log(chunk)
+           body.push(chunk)
+        })
+        return req.on('end', ()=>{
+           const parsedBody = Buffer.concat(body).toString()
+           //console.log(parsedBody)
+           const message = parsedBody.split('=')[1]
+           fs.writeFile('message.txt', message, (err)=>{
+             res.statusCode = 302
+             res.setHeader('Location','/')
+             return res.end()
+           })
+           
+        }) 
+
     }
-    else  if(url==='/node')
-    {
-    res.write('<html>')
-    res.write('<head><title>First Program</title></head>')
-    res.write('<body><h1>Welcome to my Node JS Project</h1></body>')
-    res.write('</html')
-    return res.end()
-    }
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>First Program</title></head>')
-    res.write('<body>My Name is Aaquib Rais and I am 9.5 rated individual</body>')
-    res.write('</html')
-    res.end()
+    
+    // res.setHeader('Content-Type','text/html')
+    // res.write('<html>')
+    // res.write('<head><title>First Program</title></head>')
+    // res.write('<body>My Name is Aaquib Rais and I am 9.5 rated individual</body>')
+    // res.write('</html')
+    // res.end()
     
 })
 
