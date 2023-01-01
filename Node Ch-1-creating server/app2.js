d4d6173264f4da890af675d60f0e2f50d3a1402b
DevAaqui <aaquibrais12345@gmail.com>
2023-01-01 13:42:41 +0530

    multiple request & response using localhost

--- Node Ch-1-creating server/app2.js
+++ Node Ch-1-creating server/app2.js
@@ -1,9 +1,35 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log('Aaquib')
+    //console.log(req.url, req.method, req.headers)
+    const url = req.url
+    if(url==='/home')
+    {
     res.write('<html>')
-    res.write('<head><title>First Message</title></head>')
+    res.write('<head><title>First Program</title></head>')
+    res.write('<body><h1>Welcome Home</h1></body>')
+    res.write('</html')
+    return res.end()
+    }
+    else  if(url==='/about')
+    {
+    res.write('<html>')
+    res.write('<head><title>First Program</title></head>')
+    res.write('<body><h1>Welcome to About Us Page</h1></body>')
+    res.write('</html')
+    return res.end()
+    }
+    else  if(url==='/node')
+    {
+    res.write('<html>')
+    res.write('<head><title>First Program</title></head>')
+    res.write('<body><h1>Welcome to my Node JS Project</h1></body>')
+    res.write('</html')
+    return res.end()
+    }
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>First Program</title></head>')
     res.write('<body>My Name is Aaquib Rais and I am 9.5 rated individual</body>')
     res.write('</html')
     res.end()
