783cfd81811b8a81f96c52c2f8d44d7e6a88f11c
DevAaqui <aaquibrais12345@gmail.com>
2023-01-02 12:04:26 +0530

    Code with different folder to import connections

--- Node Ch-1-creating server/app2.js
+++ Node Ch-1-creating server/app2.js
@@ -1,55 +1,7 @@
 const http = require('http')
-const fs = require('fs')
 
-const server = http.createServer((req,res)=>{
-    //console.log(req.url, req.method, req.headers)
-    const url = req.url
-    const method = req.method
-    if(url==='/')
-    {
-        fs.readFile('message.txt',{encoding: "utf-8"},(err,data)=>{
-            if(err){
-                console.log(err)
-            }
-            console.log(data)
-            res.write('<html>')
-            res.write('<head><title>First Message</title></head>')
-            res.write(`<body>${data}</body>`)
-            res.write('<body><form action="/message" method="POST"><input type="text" name="msg"><button type="submit">Send</button</form></body>')
-            res.write('</html')
-            return res.end()
+const routes = require('./routes')
 
-        })
-    
-    }
-    if(url === '/message' && method ==='POST')
-    {
-        const body = []
-        req.on('data', (chunk)=>{
-           console.log(chunk)
-           body.push(chunk)
-        })
-        return req.on('end', ()=>{
-           const parsedBody = Buffer.concat(body).toString()
-           //console.log(parsedBody)
-           const message = parsedBody.split('=')[1]
-           fs.writeFile('message.txt', message, (err)=>{
-             res.statusCode = 302
-             res.setHeader('Location','/')
-             return res.end()
-           })
-           
-        }) 
-
-    }
-    
-    // res.setHeader('Content-Type','text/html')
-    // res.write('<html>')
-    // res.write('<head><title>First Program</title></head>')
-    // res.write('<body>My Name is Aaquib Rais and I am 9.5 rated individual</body>')
-    // res.write('</html')
-    // res.end()
-    
-})
+const server = http.createServer(routes)
 
 server.listen(4000)
