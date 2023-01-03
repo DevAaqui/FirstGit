f119c5bdc98fead8a30246a131a4611a505dc261
DevAaqui <aaquibrais12345@gmail.com>
2023-01-03 20:41:54 +0530

    Adding route, body parsing

--- Node Ch-1-creating server/app.js
+++ Node Ch-1-creating server/app.js
@@ -1,15 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+//const { urlencoded } = require('body-parser')
 
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log('I am in middleware')
-    next()
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req,res,next)=> {
+    
+    res.send(`<form action="/product" method="POST"><input type="text" name="prod">
+    <select name="size">
+    <option>L</option>
+    <option>XL</option>
+    </select><button type="submit">Add</button></form>`)
 })
 
-app.use((req,res,next)=>{
-    console.log('I am in another middleware')
-    res.send('<h1>Hello from Express</h1>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
+    
+    res.send('<h1>Hello from Express JS</h1>')
 })
 
 app.listen(4000)
