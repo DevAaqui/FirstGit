cc5e467fce62cdc639e9f2ab5321bffbbfebe914
DevAaqui <aaquibrais12345@gmail.com>
2023-01-03 23:25:59 +0530

    Express Routes, 404 error,filtering paths

--- Node Ch-1-creating server/app.js
+++ Node Ch-1-creating server/app.js
@@ -4,24 +4,16 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product',(req,res,next)=> {
-    
-    res.send(`<form action="/product" method="POST"><input type="text" name="prod">
-    <select name="size">
-    <option>L</option>
-    <option>XL</option>
-    </select><button type="submit">Add</button></form>`)
-})
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    
-    res.send('<h1>Hello from Express JS</h1>')
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
 app.listen(4000)
